49c572aed81d24559fdfaec98114797722b92965
Saeed <durrani.saeed1@gmail.com>
2024-03-26 00:11:36 -0400

    initial

--- myapp/models.py
+++ myapp/models.py
@@ -3,8 +3,11 @@ from django.db import models
 
 class MyUser(AbstractUser):
     # Add any additional fields you want for your user model
+    name = models.CharField(max_length=255)
     age = models.IntegerField()
     bio = models.TextField()
+    location = models.CharField(max_length=255)
+    notification = models.BooleanField(default=False)
 
     # You can also add any custom methods or properties specific to your user model
     # For example:
